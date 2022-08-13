f48e7b19c5aa7bed38ac3bf00a707873723922e4
Abraar-x13 <Abraar13@proton.me>
2022-08-14 04:48:33 +0600

    Task 2

---
+++ Assignments Done/C Sharp/Task 2/3. Visual Studio Whole Project/Hello World/Hello World/Massage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Message
+{
+    private string _text;
+
+    public Message(string text)
+    {
+        _text = text;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine(_text);
+    }
+}
+
+
+
---
+++ Assignments Done/C Sharp/Task 2/3. Visual Studio Whole Project/Hello World/Hello World/NameTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Linq;
+namespace HelloWorld
+{
+    internal class NameTest
+    {
+        string _name;
+        Message m;
+        public NameTest(string name)
+        {
+            _name = name;
+            Greet();
+        }
+        public void Greet ()
+        {
+            bool areEqual = String.Equals(_name, "Meem", StringComparison.OrdinalIgnoreCase);
+            if (areEqual)
+            {
+                m = new Message("Welcome Back"); m.Print(); return;
+            }
+            areEqual = String.Equals(_name, "Abraar", StringComparison.OrdinalIgnoreCase);
+            if (areEqual)
+            {
+                m = new Message("What a lovely name"); m.Print(); return;
+            }
+            areEqual = String.Equals(_name, "Ian", StringComparison.OrdinalIgnoreCase);
+            if (areEqual)
+            {
+                m = new Message("Great name"); m.Print(); return;
+            }
+            areEqual = String.Equals(_name, "Shanto", StringComparison.OrdinalIgnoreCase);
+            if (areEqual)
+            {
+                m = new Message("Oh hi!"); m.Print(); return;
+            }
+            areEqual = String.Equals(_name, "Gomez", StringComparison.OrdinalIgnoreCase);
+            if (areEqual)
+            {
+                m = new Message("Beautiful Name!"); m.Print(); return;
+            }
+
+            m = new Message("That is a silly name");
+            m.Print();
+            return;
+        }
+    }
+}
